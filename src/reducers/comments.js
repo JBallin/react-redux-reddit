88b7b11e8e320e9097fcf61cbcb74a9f6b57bccc
JBallin <JBallin@users.noreply.github.com>
2018-11-03 17:31:37 -0700

    Build comments and posts reducers for fetching and creating

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,7 +1,19 @@
+import {
+  ADD_COMMENT,
+  FETCH_COMMENTS_SUCCESS,
+  FETCH_COMMENTS_FAILED,
+} from '../actions/comments';
+
 const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_COMMENTS_SUCCESS:
+      return [...action.comments];
+    case FETCH_COMMENTS_FAILED:
+      return action.comments;
+    case ADD_COMMENT:
+      return [...state, action.comment];
     default:
       return state;
   }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,19 @@
+import {
+  ADD_POST,
+  FETCH_POSTS_SUCCESS,
+  FETCH_POSTS_FAILED,
+} from '../actions/posts';
+
 const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_POSTS_SUCCESS:
+      return [...action.posts];
+    case FETCH_POSTS_FAILED:
+      return action.posts;
+    case ADD_POST:
+      return [...state, action.post]
     default:
       return state;
   }
