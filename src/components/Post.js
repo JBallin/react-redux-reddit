bc8564509981a2cebb6b33aeca43fab65fef5331
JBallin <JBallin@users.noreply.github.com>
2018-11-03 21:23:23 -0700

    Make comment form controlled, required, and bound to addComment action

--- src/components/Post.js
+++ src/components/Post.js
@@ -18,43 +18,76 @@ import FaArrowUp from 'react-icons/lib/fa/arrow-up'
 import FaArrowDown from 'react-icons/lib/fa/arrow-down'
 import FaComment from 'react-icons/lib/fa/comment'
 import Moment from 'react-moment'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { addComment } from '../actions/comments'
 
-const Post = ({ post, comments }) => {
-  const { id, author, content, title, createdAt, img_url, votes } = post;
-  const commentsList = comments.map(c => <li>{c.content}</li>);
-  const commentsCounter = comments.length === 1 ? '1 Comment' : `${comments.length} Comments`;
-  return (
-    <Row className="mt-3">
-      <Col>
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src={img_url}
-            alt="Card image cap"
-          />
-          <CardBody>
-            <CardTitle>{ title } | <FaArrowUp /> {votes} <FaArrowDown /></CardTitle>
-            <CardSubtitle>{ author }</CardSubtitle>
-            <CardText>
-              { content }
-            </CardText>
-            <hr />
-            <Moment fromNow>{createdAt}</Moment> | <FaComment /> { commentsCounter }
-            <Form inline>
-              <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                <Input type="text" name="comment" id="comment-field" placeholder="Enter a comment here" />
-              </FormGroup>
-              <Button>Submit</Button>
-            </Form>
-            <ul className="mt-2">
-              { commentsList }
-            </ul>
-          </CardBody>
-        </Card>
-      </Col>
-    </Row>
-  )
+class Post extends React.Component {
+  state = {
+    comment: '',
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addComment({
+      content: this.state.comment,
+      post_id: this.props.post.id,
+    });
+    this.setState({ comment: '' });
+  }
+
+  render() {
+    const { post, comments } = this.props;
+    const { id, author, content, title, createdAt, img_url, votes } = post;
+    const commentsList = comments.map(c => <li>{c.content}</li>);
+    const commentsCounter = comments.length === 1 ? '1 Comment' : `${comments.length} Comments`;
+    const handleSubmit = e => {
+      e.preventDefault();
+      addComment()
+    }
+    return (
+      <Row className="mt-3">
+        <Col>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src={img_url}
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>{ title } | <FaArrowUp /> {votes} <FaArrowDown /></CardTitle>
+              <CardSubtitle>{ author }</CardSubtitle>
+              <CardText>
+                { content }
+              </CardText>
+              <hr />
+              <Moment fromNow>{createdAt}</Moment> | <FaComment /> { commentsCounter }
+              <Form inline onSubmit={ this.handleSubmit }>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Input
+                    type="text"
+                    name="comment"
+                    id="comment-field"
+                    placeholder="Enter a comment here"
+                    required
+                    value={ this.state.comment }
+                    onChange={ e => this.setState({ comment: e.target.value }) }
+                  />
+                </FormGroup>
+                <Button>Submit</Button>
+              </Form>
+              <ul className="mt-2">
+                { commentsList }
+              </ul>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    )
+  }
 }
 
-export default Post
+const mapDispatchToProps = dispatch => bindActionCreators({ addComment }, dispatch);
+
+export default connect(null, mapDispatchToProps)(Post);
