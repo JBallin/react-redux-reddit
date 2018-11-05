8e7c56a40fddfb83bc718c9d4e507efac6bbd5b9
JBallin <JBallin@users.noreply.github.com>
2018-11-05 12:23:11 -0800

    Only use logger in development

--- src/store.js
+++ src/store.js
@@ -3,7 +3,12 @@ import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
 
+const middleware = [thunkMiddleware];
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
+
 export default () => createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware, logger)
+  applyMiddleware(...middleware)
 );
