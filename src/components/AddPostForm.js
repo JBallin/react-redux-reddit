a6b01560e9e5a7b9d61b81c1ed3cb1b5059e9059
JBallin <JBallin@users.noreply.github.com>
2018-11-05 09:54:18 -0800

    Organize form fields in vars

--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -24,58 +24,78 @@ class AddPostForm extends Component {
   render() {
     const isFormFilled = this.state.title && this.state.content && this.state.author;
 
+    const titleField = (
+      <FormGroup>
+        <Label for="title-field">Title *</Label>
+        <Input
+          type="text"
+          name="title"
+          id="title-field"
+          value={ this.state.title }
+          onChange={ e => this.setState({ title: e.target.value }) }
+          required
+        />
+      </FormGroup>
+    );
+
+    const bodyField = (
+      <FormGroup>
+        <Label for="body-field">Body *</Label>
+        <Input
+          type="text"
+          name="body"
+          id="body-field"
+          value={ this.state.content }
+          onChange={ e => this.setState({ content: e.target.value }) }
+          required
+        />
+      </FormGroup>
+    );
+
+    const authorField = (
+      <FormGroup>
+        <Label for="author-field">Author *</Label>
+        <Input
+          type="text"
+          name="author"
+          id="author-field"
+          value={ this.state.author }
+          onChange={ e => this.setState({ author: e.target.value }) }
+          required
+        />
+      </FormGroup>
+    );
+
+    const imageField = (
+      <FormGroup>
+        <Label for="image-field">Image URL</Label>
+        <Input
+          type="url"
+          name="image"
+          id="image-field"
+          value={ this.state.img_url }
+          onChange={ e => this.setState({ img_url: e.target.value }) }
+        />
+      </FormGroup>
+    );
+
+    const createPostButton = (
+      <Button disabled={!isFormFilled} type="submit">Create Post</Button>
+    )
+
     return (
       <Row>
         <Col sm="10">
           <Form onSubmit={this.handleSubmit}>
-            <FormGroup>
-              <Label for="title-field">Title *</Label>
-              <Input
-                type="text"
-                name="title"
-                id="title-field"
-                value={ this.state.title }
-                onChange={ e => this.setState({ title: e.target.value }) }
-                required
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="body-field">Body *</Label>
-              <Input
-                type="text"
-                name="body"
-                id="body-field"
-                value={ this.state.content }
-                onChange={ e => this.setState({ content: e.target.value }) }
-                required
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="author-field">Author *</Label>
-              <Input
-                type="text"
-                name="author"
-                id="author-field"
-                value={ this.state.author }
-                onChange={ e => this.setState({ author: e.target.value }) }
-                required
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="image-field">Image URL</Label>
-              <Input
-                type="url"
-                name="image"
-                id="image-field"
-                value={ this.state.img_url }
-                onChange={ e => this.setState({ img_url: e.target.value }) }
-              />
-            </FormGroup>
-            <Button disabled={!isFormFilled} type="submit">Create Post</Button>
+            { titleField }
+            { bodyField }
+            { authorField }
+            { imageField }
+            { createPostButton }
           </Form>
         </Col>
       </Row>
-    )
+    );
   }
 }
 
